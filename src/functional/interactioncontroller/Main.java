d67eeac22613fabd21da7e324cd95394f579ea17
EdsonMartinelli <martinelli1997@gmail.com>
2022-12-13 23:14:04 -0300

    fix: description and folder name of interaction controller

--- src/functional/interactioncontroller/Main.java
+++ src/functional/interactioncontroller/Main.java
@@ -1,18 +1,25 @@
-package src.functional.usercontrol;
+package src.functional.interactioncontroller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
-    List<List<String>> table = new ArrayList<>();
+
+    public static void main(String[] args){
+        List<List<String>> table = new ArrayList<>();
         table.add(Arrays.asList("User1", "User2", "2"));
         table.add(Arrays.asList("User3", "User4", "6"));
         table.add(Arrays.asList("User2", "User4", "14"));
         table.add(Arrays.asList("User4", "User1", "3"));
 
+        /*
+         * Using a third hashmap to store data.
+         */
+        /* 
         Map<String, Integer> mapstring1 = table.stream().collect(Collectors.toMap(s -> { return s.get(0); },
                                                                                   s -> { return -(Integer.parseInt(s.get(2))); },
                                                                                   (s, a) -> { return s + a; }
@@ -29,15 +36,9 @@ public class Main {
             mapstring3.merge(k, v, (old_value, new_value) -> {return old_value + new_value;});
         });
 
-        //Map<String, Integer> result= mapstring3.entrySet().stream().filter(item -> item.getValue() < 0).collect(Collectors.toMap(x -> x.getKey(), x -> x.getValue()));
-
-        System.out.println(mapstring1);
-        System.out.println(mapstring2);
         System.out.println(mapstring3);
-        //System.out.println(result);
+        */
 
-
-        System.out.println("---------------------------------------------------");
         Map<String, Integer> resultMap = new HashMap<>();
         table
         .stream()
@@ -60,4 +61,6 @@ public class Main {
                  ));
 
         System.out.println(resultMap);
+    }
+    
 }
---
+++ src/functional/interactioncontroller/description.md
@@ -0,0 +1,14 @@
+# Interaction Controller
+
+In this project users going to interact each others and the information about the interaction will be store in a row with names of two users and a value. All users starting with 0 in the score, each row the first name has your score decreased by the row value field and the second name has your value increased by the value.
+
+At the end, the final value of all users is stored in a hashmap where the key is the name of user e the value is the final score.
+
+## Table of Interactions
+
+| First | Second | Value |
+|-------|--------|-------|
+| User1 | User2  | 2     |
+| User3 | User4  | 6     |
+| User2 | User4  | 14    |
+| User3 | User1  | 3     |
